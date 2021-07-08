ada8abd055e159523fc689ea97a165ffea7f87f6
warrior14 <ironaddicts14@gmail.com>
2021-07-07 21:19:02 -0500

    added data provider module and animal provider component refactored aninmal card component to display data from the api database as HTML

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
-import { AnimalCard } from "./animals/AnimalCard.js"
 import { EmployeeCard } from "./employees/Employee.js"
 import { LocationCard } from "./locations/Location.js"
 import { CustomerCard } from "./customers/Customers.js"
+import { AnimalProvider } from "./animals/AnimalProvider"
+import { AnimalList } from "./animals/AnimalList"
 
 
 export const ApplicationViews = () => {
@@ -16,9 +17,11 @@ export const ApplicationViews = () => {
             </Route>
 
             {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
-                <AnimalCard />
-            </Route>
+            <AnimalProvider>
+                <Route exact path="/animals">
+                    <AnimalList />
+                </Route>
+            </AnimalProvider>
 
             <Route path="/employees">
                 <EmployeeCard />
